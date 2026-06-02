88e7c0f0cd3523f76f80ac760ad36d8b60c53acd
Samson Leong (wkstn) <55839002+SSL32081@users.noreply.github.com>
2026-06-03 00:17:46 +0800

    Add source

--- utils/files_io.py
+++ utils/files_io.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
+
+# These are adapted from: https://stackoverflow.com/a/29130146
 def h5store(filename, df):
     with pd.HDFStore(filename) as store:
         store.put('df', df)
         store.get_storer('df').attrs.metadata = df.attrs
+
 
 def h5load(filename):
     with pd.HDFStore(filename) as store:
